464ffee8b6839895d9a5491f16b4de44ff4628ec
Raul <insepal1998@gmail.com>
2021-01-29 19:40:51 +0100

    Login Layout & funcionamiento basico

--- app/src/main/java/com/car_pa_ra/hidra/Login.java
+++ app/src/main/java/com/car_pa_ra/hidra/Login.java
@@ -2,13 +2,33 @@ package com.car_pa_ra.hidra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Login extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        ImageView imgLogin;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+            imgLogin = findViewById(R.id.imgLogin);
+
+            Glide.with(this).load(R.drawable.ic_icon).circleCrop().into(imgLogin);
+        }
+
+
+    public void btnEntrar(View view) {
+            Intent iExplora = new Intent(this, MainActivity.class);
+            startActivity(iExplora);
+    }
+
+    public void btnRegistro(View view) {
+            Intent iRegis = new Intent(this, Registro.class);
+            startActivity(iRegis);
     }
 }
