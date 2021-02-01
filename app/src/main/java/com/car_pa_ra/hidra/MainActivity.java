970cb98901131745a4cba9765d0df50cb94a77c2
Pablo <pablopezgarcia97@gmail.com>
2021-02-01 19:39:00 +0100

    Creando main

--- app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
+++ app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String text = "Hola";
+        //HOLA
     }
 
     public void aa(View view) {
