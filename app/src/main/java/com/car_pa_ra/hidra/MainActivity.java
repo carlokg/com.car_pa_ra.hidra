40b24776b07d77f8a78ee0c19277c1619146d173
Pablo <pablopezgarcia97@gmail.com>
2021-02-04 18:57:54 +0100

    Ajustando colores

--- app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
+++ app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
