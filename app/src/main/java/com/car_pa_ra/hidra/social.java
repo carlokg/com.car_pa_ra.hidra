69b0964274c8f28477670bb9cced54ed0fdb56e1
carlokg <carlokg89@gmail.com>
2021-02-05 11:32:56 +0100

    6666

--- app/src/main/java/com/car_pa_ra/hidra/social.java
+++ app/src/main/java/com/car_pa_ra/hidra/social.java
@@ -7,4 +7,5 @@ public class social {
     ///hola de nuevo nuevo nuevo
     ///camion de camiones camiones camiones
     //camion de camiones camiones camiones camiones camiones camiones camiones camones camiones camiones camiones camiones hola cara cola
+    //decamiones
 }
