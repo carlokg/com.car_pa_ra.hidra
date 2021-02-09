339891b12db140717ce50acfc5b28538154120cf
Pablo <pablopezgarcia97@gmail.com>
2021-02-09 19:57:14 +0100

    About us

--- app/src/main/java/com/car_pa_ra/hidra/AjustesFragment.java
+++ app/src/main/java/com/car_pa_ra/hidra/AjustesFragment.java
@@ -7,13 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-public class AjustesFragment extends Fragment {
+public class AjustesFragment extends Fragment implements View.OnClickListener{
 
     ImageView imgAjustes;
+    Button btnAboutus;
 
     public AjustesFragment() {
     }
@@ -24,8 +26,6 @@ public class AjustesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
     }
 
     @Override
@@ -35,8 +35,42 @@ public class AjustesFragment extends Fragment {
         imgAjustes = view.findViewById(R.id.imgAjustes);
         Glide.with(this).load(R.drawable.ic_appbar_config).circleCrop().into(imgAjustes);
 
+        btnAboutus = (Button) view.findViewById(R.id.btnAbout);
+        btnAboutus.setOnClickListener(this);
+
         return view;
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO TERMINAR ONCLICK
+       /* Fragment selectedFragment = null;
+        switch (v.getId()) {
+            case R.id.btnAbout:
+
+                AboutFragment abf = new AboutFragment();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, abf)
+                        .commit();
+                //selectedFragment = new AboutFragment();
+
+                break;
+            case R.id.btnHelp:
+                break;
+
+            case R.id.btnLogout:
+                break;
+
+
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, selectedFragment)
+                .commit();*/
 
     }
 }
