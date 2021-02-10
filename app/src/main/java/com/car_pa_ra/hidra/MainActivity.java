f731d17e9c05a4d243ce3295c3d9bd69b6f9aee0
carlokg <carlokg89@gmail.com>
2021-02-10 08:58:54 +0100

    Gradle

--- app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
+++ app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
@@ -4,11 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
--- app/src/main/java/com/car_pa_ra/hidra/social.java
+++ app/src/main/java/com/car_pa_ra/hidra/social.java
@@ -1,11 +1,6 @@
 package com.car_pa_ra.hidra;
 
 public class social {
-    //hola
-    //hola de nuevo
-    //hola de nuevo nuevo
-    ///hola de nuevo nuevo nuevo
-    ///camion de camiones camiones camiones
-    //camion de camiones camiones camiones camiones camiones camiones camiones camones camiones camiones camiones camiones hola cara cola
-    //decamiones
+
+
 }
