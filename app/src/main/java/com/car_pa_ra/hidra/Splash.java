dcf77ee6ca1cc484b52804c8575a7f03c2a78142
Raul <insepal1998@gmail.com>
2021-02-14 14:42:16 +0100

    Añadido Fondo Animado(En pruebas)

--- app/src/main/java/com/car_pa_ra/hidra/Splash.java
+++ app/src/main/java/com/car_pa_ra/hidra/Splash.java
@@ -3,8 +3,10 @@ package com.car_pa_ra.hidra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,14 +16,19 @@ import com.bumptech.glide.Glide;
 public class Splash extends AppCompatActivity {
 
     ImageView imgApp;
+    View fondo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
         imgApp = findViewById(R.id.imgApp);
+        fondo = findViewById(R.id.fondo);
         Glide.with(this).load(R.drawable.ic_icon_claro).into(imgApp);
+        fondo.setBackgroundResource(R.drawable.splash_anim);
 
+        AnimationDrawable progressAnimation = (AnimationDrawable) fondo.getBackground();
+        progressAnimation.start();
 
         Animation elegance = AnimationUtils.loadAnimation(this, R.anim.fadeln);
         imgApp.startAnimation(elegance);
