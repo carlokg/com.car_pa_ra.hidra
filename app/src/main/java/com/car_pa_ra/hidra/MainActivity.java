11ae27a9d89c90d3e9f8af34b53d983782898604
carlokg <carlokg89@gmail.com>
2021-02-18 17:18:20 +0100

    @Author

--- app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
+++ app/src/main/java/com/car_pa_ra/hidra/MainActivity.java
@@ -15,7 +15,20 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+/**
+ * @author Raúl
+ * @author Pablo
+ * @author Carlos
+ *
+ * Clase MainActiviti contendrá la barra de navegación y sobre este activity se cargarán
+ * Todos los fragmentos de la aplicación, de tal forma que la barra estará presente en
+ * todos los Fragmentos.
+ * @see MainActivity
+ * @see AboutFragment
+ * @see AjustesFragment
+ *
 
+ */
 public class MainActivity extends AppCompatActivity
         implements OnControlerFragmentListener{
 
