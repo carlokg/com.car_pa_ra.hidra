7f9f4c9f9c012b00cd5c24ea30bd7aa3f35b3cc3
raulgarcia <insepal1998@gmail.com>
2021-02-26 12:34:25 +0100

    Fixed MapaActivity

--- app/src/main/java/com/car_pa_ra/hidra/MapaActivity.java
+++ app/src/main/java/com/car_pa_ra/hidra/MapaActivity.java
@@ -1,85 +1,64 @@
 package com.car_pa_ra.hidra;
 
-
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 
-import com.car_pa_ra.hidra.Utils.CustomMarkerInfo;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
-
 
 public class MapaActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    Location currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    private static final int REQUEST_CODE = 101;
+    String location;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        fetchLocation();
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-
+        location = getIntent().getStringExtra( "LOCATION" );
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                //TO-DO:
-                //Añadir icono
-                //.icon(BitmapDescriptorFactory.fromResource(R.drawable.perfil))
-        );
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        CustomMarkerInfo markerInfoWindowAdapter = new CustomMarkerInfo(getApplicationContext());
-        googleMap.setInfoWindowAdapter(markerInfoWindowAdapter);
+
+        LatLng latLng = new LatLng(getLocation().latitude, getLocation().longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
 
     }
 
-    private void fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
+    public LatLng getLocation(){
+        LatLng loc = null;
+
+        switch (location.toLowerCase()){
+
+            case "madrid":
+                loc = new LatLng( 40.4167, -3.70325 );
+                break;
+            case "barcelona":
+                loc = new LatLng( 41.3879, 2.16992 );
+                break;
+            case "sevilla":
+                loc = new LatLng( 37.3826, -5.99629 );
+                break;
+            case "oviedo":
+                loc = new LatLng( 43.36029, -5.84476 );
+                break;
         }
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
-                    SupportMapFragment supportMapFragment = (SupportMapFragment)
-                            getSupportFragmentManager().findFragmentById(R.id.map);
-                    assert supportMapFragment != null;
-                    supportMapFragment.getMapAsync(MapaActivity.this);
-                }
-            }
-        });
+
+        return loc;
     }
 }
